7b12be3b4b6b8c3f7f2ffde1a0fc400bbb759ae8
Jose Vitor <josevtrleal@gmail.com>
2025-11-21 00:34:18 -0300

    feat: Adição de suporte para deploy na Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-
 import App from "./App.jsx";
 import { GlobalStyle } from "./styles/global.js";
 import { theme } from "./styles/theme.js";
-
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
+
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:3333";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
